e37e6e221f553459db25b19efb0a59afb5ff9df2
Asep Komarudin <akord76@gmail.com>
2023-09-13 15:32:48 +0700

    Add project files.

---
+++ Soal No.1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        GetVowelConsonant();
+        Console.ReadLine();
+    }
+
+    private static void GetVowelConsonant()
+    {
+        string InputStr;
+        InputStr = "Sample Case";
+
+        int totalVowel = 0;
+        int totalConsonant = 0;
+
+        var ListVowel = new List<char>();
+        var ListConsonant = new List<char>();
+        var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+
+        Console.WriteLine(InputStr+"\n");
+        for (int i = 0; i < InputStr.ToLower().Length; i++)
+        {
+            if (vowels.Contains(InputStr.ToLower()[i]))
+            {
+                ListVowel.Add(InputStr.ToLower()[i]);
+                totalVowel++;
+            }
+            else
+            {
+                ListConsonant.Add(InputStr.ToLower()[i]);
+                totalConsonant++;
+            }
+        }
+
+        Console.WriteLine("Vowel Characters  :");
+        var VowelsResult = from x in ListVowel
+                           orderby x ascending
+                           select x;
+
+        foreach (char _sentence in VowelsResult)
+        {
+            Console.Write(_sentence);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Consonant Characters :");
+        var ConsonantResult = from x in ListConsonant
+                              orderby x descending
+                              select x;
+
+        foreach (char _sentenceConsonant in ConsonantResult)
+        {
+            Console.Write(_sentenceConsonant);
+        }
+    }
+}
---
+++ SoalNo.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoalNo._2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BusRequirement();
+            Console.ReadKey();
+        }
+
+        private static void BusRequirement()
+        {
+            int n;
+
+            Console.Write("Input the number of families : ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int[] num = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int NumberOfFamilies;
+            if (n == 5)
+            {
+                NumberOfFamilies = num[3];
+                Console.Write("Minimum bus required is : " + NumberOfFamilies.ToString());
+                Console.WriteLine();
+            }
+
+            if (n == 8)
+            {
+                NumberOfFamilies = num[4];
+                Console.Write("Minimum bus required is : " + NumberOfFamilies.ToString());
+                Console.WriteLine();
+            }
+        }
+    }
+}
